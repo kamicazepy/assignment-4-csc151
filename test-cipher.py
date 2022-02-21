4d246ab8c6302e452b49b24de2cab8d7cba25d1a
Camila Maldonado Franco <cmaldonadofranco@smith.edu>
2022-02-20 20:14:01 -0500

    Update test-cipher.py for the second time. Made new comments and new edge tests.

--- test-cipher.py
+++ test-cipher.py
@@ -1,25 +1,35 @@
 from cipher import*
 
-#Functions to test functions from cipher.py program.
+#Testing shift letter function giving 3 as the shift number which will encode the letter based on the ASCII code system.
 def test_shiftLetterby3():
     assert shiftLetter("a",3) == "d"
     
-
+#Testing shift letter by 0, which should output the same letter inserted since 0 will not make any shift changes. 
 def test_shiftLetterby0():
     assert shiftLetter("b",0) == "b"
 
-#Function to test encryption from cipher.py program.
+#Function should get a word/message to encrypt and use the random uppercase pad generated to encrypt it and viceversa for the decryption function. 
+#For this test I used the example given on the caesar cesar one time pad example. 
+
 def test_encrypts():
-    assert encrypts("R", "Q") == "H"
+    assert encrypts("Rally", "QWERT") == "Hwpcr"
+    #Edge case: The program should be able to skip all blank spaces and punctuation signs that are not lowercase or uppercase letters.
+def test_encryptspunctuation():
     assert encrypts("!@*!,","") == "!@*!,"
 
 def test_decrypts():
     assert decrypts("Hwpcr","QWERT") == "Rally"
+def test_decrypts_punctuation():
     assert decrypts("!@*!,", "") == "!@*!,"
 
 
-#Confused on how to use random_Upper as an argument for generate pad function
-def test_generatePad():
-    assert len(generatePad(8)) == 8 
+#Since the generatePad() function takes the length of the message and counts how many characters it has to make a pad that has the same numbers of letters as the message,
+#the the output of the function should be the same number of characters as the number of characters of the length. 
+def test_generatePadlen():
+    assert len(generatePad(5)) == 5 
     assert len(generatePad(0)) == 0
+
+#Tests that each pad that is generated gives a unique random pad.
+def test_generatePadunique():
+    assert generatePad(150) != generatePad(150)
 
